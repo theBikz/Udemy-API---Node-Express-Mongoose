8c07e262b6818aaa3b5fa759e8f82896a6413009
Bipin Kuriakose <bpnmnu@gmail.com>
2020-12-27 22:17:34 +0530

    get method issue resolved

--- db/udemy.js
+++ db/udemy.js
@@ -2,25 +2,25 @@ const model = require("../models/udemy");
 var settings = require("../db/settings");
 
 exports.save = function(data, callback) {
-    new model.vacations(data).save(function(err, inserted) {
+    new model.Course(data).save(function(err, inserted) {
         callback(err, inserted)
     })
 }
 
 exports.saveMany = function(rows, callback) {
-    model.Vacations.insertMany(rows, function(err, docs) {
+    model.Course.insertMany(rows, function(err, docs) {
         callback(err, docs)
     })
 }
 
 exports.update = function(criteria, doc, callback) {
-    model.Vacations.updateMany(criteria, doc, function(err, data){
+    model.Course.updateMany(criteria, doc, function(err, data){
         callback(err, data)
     })
 }
 
 exports.select = function(criteria, callback) {
-    model.Vacations.find(criteria, function(err, data) {
+    model.Course.find(criteria, function(err, data) {
         callback(err, data)
     })
 }
